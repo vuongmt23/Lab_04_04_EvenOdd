d437132cf2c5bc9bc4092352b076dc8c175686b6
Minh Vuong <vuongmt@mail.uc.edu>
2024-09-23 11:22:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 17;
+
+        if (numToExamine % 2 == 0) {
+            System.out.println("The number " + numToExamine + " is even.");
+        } else {
+            System.out.println("The number " + numToExamine + " is odd.");
+        }
+    }
+}
